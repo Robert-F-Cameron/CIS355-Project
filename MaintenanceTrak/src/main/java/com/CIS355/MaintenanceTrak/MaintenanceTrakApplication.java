0132902735a65845b8ece23b98ed50ae3f011e60
Robert-F-Cameron <robert.f.cameron@gmail.com>
2021-09-02 20:29:54 -0700

    Updated Read Me

--- MaintenanceTrak/src/main/java/com/CIS355/MaintenanceTrak/MaintenanceTrakApplication.java
+++ MaintenanceTrak/src/main/java/com/CIS355/MaintenanceTrak/MaintenanceTrakApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MaintenanceTrakApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Hello World!");
 		SpringApplication.run(MaintenanceTrakApplication.class, args);
 	}
 
