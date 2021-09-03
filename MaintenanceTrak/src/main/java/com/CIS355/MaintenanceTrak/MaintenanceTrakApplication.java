b5cfa64e1e562f9bf29930239c35ae07342ab21c
Robert-F-Cameron <robert.f.cameron@gmail.com>
2021-09-02 20:19:35 -0700

    initial spring application with Spring JPA and H2 embedded

---
+++ MaintenanceTrak/src/main/java/com/CIS355/MaintenanceTrak/MaintenanceTrakApplication.java
@@ -0,0 +1,13 @@
+package com.CIS355.MaintenanceTrak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaintenanceTrakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaintenanceTrakApplication.class, args);
+	}
+
+}
---
+++ MaintenanceTrak/src/test/java/com/CIS355/MaintenanceTrak/MaintenanceTrakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CIS355.MaintenanceTrak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaintenanceTrakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
