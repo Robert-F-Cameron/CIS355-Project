346565ad198d0fb14812342658ec45325a5d2148
Robert-F-Cameron <robert.f.cameron@gmail.com>
2021-09-08 21:02:46 -0700

    started GUI

---
+++ MaintenanceTrak/src/main/java/com/CIS355/MaintenanceTrak/UI/Forms/EquipmentForm.java
@@ -0,0 +1,11 @@
+package com.CIS355.MaintenanceTrak.UI.Forms;
+
+import javax.swing.*;
+
+public class EquipmentForm {
+    private JTable equipmentTable;
+    private JPanel equipmentPanel;
+    private JScrollPane equipmentPanelScroll;
+
+
+}
